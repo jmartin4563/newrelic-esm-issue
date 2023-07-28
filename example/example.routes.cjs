0a70f4138777d055c1574b6b7b4fb17210ee9fb3
Jessica Lopatta <jlopatta@newrelic.com>
2023-07-28 17:15:43 -0400

    committing work thus far

--- example/example.routes.cjs
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const exampleController = require('./example.controller.cjs')
-
-const router = express.Router();
-
-router.get('/looking/path', exampleController.doTest);
-
-module.exports = router;
